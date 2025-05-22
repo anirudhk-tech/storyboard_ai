ce0a0e9e5d25774fc7175e00dfb3d4c11961bd9c
Anirudh Kuppili <anirudhkuppili@gmail.com>
2025-05-22 16:34:50 +0530

    Outputting Elastic IP

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "app_public_dns" {
   description = "Public DNS name of the EC2 running your app"
   value       = aws_instance.app.public_dns
 }
+
+output "app_public_ip" {
+  description = "Elastic IP of the app"
+  value       = aws_eip.app_eip.public_ip
+}
