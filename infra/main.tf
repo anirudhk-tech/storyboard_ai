dd135ac65a0648ffb4c067710308d6427bf62212
Anirudh Kuppili <anirudhkuppili@gmail.com>
2025-05-23 08:11:59 +0530

    Opened port 30080 to traffic

--- infra/main.tf
+++ infra/main.tf
@@ -54,6 +54,13 @@ resource "aws_security_group" "app_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "NodePort for nginx"
+    from_port   = 30080
+    to_port     = 30080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
